6091838744252492c4990a956d310e5fd9fb2c16
Michael Peter <michael_piterct@hotmail.com>
2021-11-06 18:09:42 -0300

    delete

--- Blog/Blog/Program.cs
+++ Blog/Blog/Program.cs
@@ -14,11 +14,17 @@ namespace Blog
                 //context.Tags.Add(tag);
                 //context.SaveChanges();
 
-                var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
-                tag.Name = ".NET";
-                tag.Slug = "dotnet";
-                context.Update(tag);
-                context.SaveChanges();
+
+                //var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+                //tag.Name = ".NET";
+                //tag.Slug = "dotnet";
+                //context.Update(tag);
+                //context.SaveChanges();
+
+
+                //var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+                //context.Remove(tag);
+                //context.SaveChanges();
             }
         }
     }
