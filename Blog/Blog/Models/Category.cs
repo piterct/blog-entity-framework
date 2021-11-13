6d959b37222aa74da863174ac1a9f49c29a222d5
Michael Peter <michael_piterct@hotmail.com>
2021-11-13 18:54:36 -0300

    category Required, MinLength, MaxLength and Column Type

--- Blog/Blog/Models/Category.cs
+++ Blog/Blog/Models/Category.cs
@@ -9,7 +9,15 @@ namespace Blog.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(80)]
+        [Column("Name", TypeName = "NVARCHAR")]
         public string Name { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(80)]
+        [Column("Name", TypeName = "VARCHAR")]
         public string Slug { get; set; }
     }
 }
