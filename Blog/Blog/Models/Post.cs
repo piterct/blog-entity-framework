16fca454767bdd95413b94b02c4895eb4a8dfa46
Michael Peter <michael_piterct@hotmail.com>
2021-11-13 19:01:44 -0300

    Navigation Properties

--- Blog/Blog/Models/Post.cs
+++ Blog/Blog/Models/Post.cs
@@ -1,17 +1,26 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Blog.Models
 {
+    [Table("Post")]
     public class Post
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public int CategoryId { get; set; }
-        public int AuthorId { get; set; }
         public string Title { get; set; }
         public string Sumary { get; set; }
         public string Body { get; set; }
         public string Slug { get; set; }
         public DateTime CreateDate { get; set; }
         public DateTime LastUpdateDate { get; set; }
+        [ForeignKey("CategoryId")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        [ForeignKey("AuthorId")]
+        public int AuthorId { get; set; }
+        public User Author { get; set; }
     }
 }
--- Blog/Blog/Models/User.cs
+++ Blog/Blog/Models/User.cs
@@ -1,7 +1,13 @@
-﻿namespace Blog.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog.Models
 {
+    [Table("User")]
     public class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
