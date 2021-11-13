7def85d4aa7ffcaf06ff0829293d5e77c64e8ad2
Michael Peter <michael_piterct@hotmail.com>
2021-11-13 19:59:50 -0300

    using include like inner join entity ef

--- Blog/Blog/Program.cs
+++ Blog/Blog/Program.cs
@@ -15,40 +15,47 @@ namespace Blog
             {
                 using var context = new BlogDataContext();
 
-                var user = new User
-                {
-                    Name = "Michael Peter",
-                    Slug = "michaelpeter",
-                    Email = "michael.peter@developer.com",
-                    Bio = ".NET Developer ",
-                    Image = "https://imagem.com",
-                    PasswordHash = "123098457"
-                };
+                //var user = new User
+                //{
+                //    Name = "Michael Peter",
+                //    Slug = "michaelpeter",
+                //    Email = "michael.peter@developer.com",
+                //    Bio = ".NET Developer ",
+                //    Image = "https://imagem.com",
+                //    PasswordHash = "123098457"
+                //};
 
-                var category = new Category
-                {
-                    Name = "Backend",
-                    Slug = "backend"
-                };
+                //var category = new Category
+                //{
+                //    Name = "Backend",
+                //    Slug = "backend"
+                //};
 
-                var post = new Post
-                {
-                    Author = user,
-                    Category = category,
-                    Body = "<p>Hello world</p>",
-                    Slug = "Ef-Core",
-                    Summary = "In this post you will learn Ef core",
-                    Title = "Beginning  Ef Core",
-                    CreateDate = DateTime.Now,
-                    LastUpdateDate = DateTime.Now
-                };
+                //var post = new Post
+                //{
+                //    Author = user,
+                //    Category = category,
+                //    Body = "<p>Hello world</p>",
+                //    Slug = "Ef-Core",
+                //    Summary = "In this post you will learn Ef core",
+                //    Title = "Beginning  Ef Core",
+                //    CreateDate = DateTime.Now,
+                //    LastUpdateDate = DateTime.Now
+                //};
 
-                PostRepository.CreatePost(post);
+                //PostRepository.CreatePost(post);
+
+                var posts = PostRepository.GetPosts();
+
+                foreach (var post in posts)
+                {
+                    Console.WriteLine($"{post.Title}  writed by {post.Author?.Name}");
+                }
             }
 
             catch (Exception ex)
             {
-                Console.WriteLine(ex.ToString());   
+                Console.WriteLine(ex.ToString());
             }
 
         }
--- Blog/Blog/Repository/PostRepository.cs
+++ Blog/Blog/Repository/PostRepository.cs
@@ -1,5 +1,8 @@
 ﻿using Blog.Data;
 using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Blog.Repository
 {
@@ -16,6 +19,18 @@ namespace Blog.Repository
             }
 
             return result == 0 ? false : true;
+        }
+
+        public static List<Post> GetPosts()
+        {
+            using (var context = new BlogDataContext())
+            {
+                return context.Posts
+                     .AsNoTracking()
+                     .Include(x => x.Author)
+                     .OrderByDescending(x => x.LastUpdateDate)
+                     .ToList();
+            }
         }
     }
 }
