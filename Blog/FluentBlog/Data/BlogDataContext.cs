241f616deecac168c08f69f0a49259f62f78ffb6
Michael Peter <michael_piterct@hotmail.com>
2021-11-14 02:25:44 -0300

    initial fluent blog

---
+++ Blog/FluentBlog/Data/BlogDataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FluentBlog.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=.\\MSSQLSERVER2019;Database=FluentBlog;User ID=local;Password=147258;");
+        }
+    }
+}
---
+++ Blog/FluentBlog/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FluentBlog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
