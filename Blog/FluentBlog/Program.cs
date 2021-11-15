fff7381d6982951fe2b9c9f6518d5ce4de6a3c07
Michael Peter <michael_piterct@hotmail.com>
2021-11-15 04:39:54 -0300

    AsNoTracking

--- Blog/FluentBlog/Program.cs
+++ Blog/FluentBlog/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace FluentBlog
+﻿using FluentBlog.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace FluentBlog
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            using var context = new BlogDataContext();
+            var posts = context.Posts.FirstOrDefault(x=> x.Id == 1);
+            var posts2 = context.Posts.AsNoTracking();
         }
     }
 }
