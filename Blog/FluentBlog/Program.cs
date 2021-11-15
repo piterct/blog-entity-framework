01383a44184ec5c3d9363a5f47ed1beab6b2f942
Michael Peter <michael_piterct@hotmail.com>
2021-11-15 03:09:33 -0300

    EF relationships test

--- Blog/FluentBlog/Program.cs
+++ Blog/FluentBlog/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FluentBlog.Data;
+using FluentBlog.Models;
+using System;
+using System.Linq;
 
 namespace FluentBlog
 {
@@ -6,7 +9,35 @@ namespace FluentBlog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var context = new BlogDataContext();
+
+            //context.Users.Add(new User
+            //{
+            //    Name = "Michael P.",
+            //    Email = "michael.p@developer.com",
+            //    Bio = "Software Developer",
+            //    Image = "https://developer.com",
+            //    Slug = "michael-p",
+            //    PasswordHash = "1234"
+            //});
+
+            //context.SaveChanges();
+
+            var user = context.Users.FirstOrDefault();
+
+            var post = new Post
+            {
+                Author = user,
+                Body = "My Article",
+                Category = new Category { Name = "Backend", Slug = "backend-ef-core" },
+                CreateDate = DateTime.Now,
+                Slug = "my-article",
+                Summary = "Beginning backend",
+                Title = "Ef Core"
+            };
+
+            context.Posts.Add(post);
+            context.SaveChanges();
         }
     }
 }
