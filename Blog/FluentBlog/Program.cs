71f2b80ecf96abcf2685785014075c50479bd3b3
Michael Peter <michael_piterct@hotmail.com>
2021-11-15 04:50:53 -0300

    async and await

--- Blog/FluentBlog/Program.cs
+++ Blog/FluentBlog/Program.cs
@@ -1,16 +1,30 @@
 ﻿using FluentBlog.Data;
+using FluentBlog.Models;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace FluentBlog
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             using var context = new BlogDataContext();
-            var posts = context.Posts.FirstOrDefault(x=> x.Id == 1);
-            var posts2 = context.Posts.AsNoTracking();
+
+            var post = await context.Posts.ToListAsync();
+            var tausersgs = await context.Users.ToListAsync();
+
+            var posts = await GetPosts(context);
+
+            Console.WriteLine("Teste");
+        }
+
+        public static async Task<IEnumerable<Post>> GetPosts(BlogDataContext context)
+        {
+           return await context.Posts.ToListAsync();
         }
     }
 }
