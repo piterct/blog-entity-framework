608d0814bf30b98fa6c66aeae66926423ee3c898
Michael Peter <michael_piterct@hotmail.com>
2021-11-15 02:45:48 -0300

    ModelBuilder

--- Blog/FluentBlog/Data/BlogDataContext.cs
+++ Blog/FluentBlog/Data/BlogDataContext.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FluentBlog.Data.Mappings;
+using FluentBlog.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FluentBlog.Data
 {
     public class BlogDataContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             options.UseSqlServer("Server=.\\MSSQLSERVER2019;Database=Blog;User ID=local;Password=147258;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.ApplyConfiguration(new PostMap());
         }
     }
 }
